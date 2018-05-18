db2a7af750a754e2e372d186e9f991b38aa2978a
Strongkong <strongkongoriginal@gmail.com>
2018-05-17 23:48:29 -0700

    Mocking tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,37 @@ import org.junit.Test;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore torpedostore;	
 
   @Before
   public void init(){
-    this.ship = new GT4500();
+    this.torpedostore = mock(TorpedoStore.class);
+    this.ship = new GT4500(torpedostore, torpedostore);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(torpedostore.fire(1)).thenReturn(true)  ;  
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(torpedostore, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(torpedostore.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(torpedostore, times(2)).fire(1);
   }
 
 }
